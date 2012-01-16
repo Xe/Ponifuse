06174b7fd9fdc797daddfe12042893ffd3c032f8
Sam Dodrill <shadow.h511@gmail.com>
2012-01-15 21:33:53 -0800

    is it the array?

--- src/ponify.py
+++ src/ponify.py
@@ -4,13 +4,15 @@ list_of_words = eval(open("list.txt", "r").read())
 
 def replace(text, replacements=list_of_words):
 	print "given:", text
+	#text.split()
 	for number in range(len(replacements)):
 		word = replacements[number][0]
 		print word, "tried"
 		if word in text:
+			print word, "found"
 			text.replace(word, replacements[number][1])
 			
-	return text
+	return str(text)
 
 def undo_replace(text, replacements=list_of_words):
         for number in range(len(replacements)):
